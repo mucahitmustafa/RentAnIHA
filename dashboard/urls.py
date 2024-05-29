a728dd9ee9b98a0f8798882d918e97f27e349dd4
mucahitmustafa <mucahitmustafay@gmail.com>
2024-05-29 17:47:28 +0300

    Design and improve backend of iha categories page.

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -8,6 +8,7 @@ urlpatterns = [
     path('members/', views.members_view, name='members'),
     path('members/<int:id>', views.member_view, name='member'),
     path('categories/', views.iha_categories_view, name='categories'),
+    path('categories/<int:id>', views.iha_category_view, name='category'),
     path('ihas/', views.ihas_view, name='ihas'),
     path('ihas/<int:id>', views.iha_view, name='iha'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,5 @@
+import json
+from django.utils import timezone
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login, authenticate
 from django.contrib.auth.forms import AuthenticationForm
@@ -94,10 +96,52 @@ def iha_categories_view(request):
     if not request.user.is_staff:
         return HttpResponse('Unauthorized', status=401)
 
-    _categories = Category.objects.filter()
-    if not _categories.exists():
-        _categories = []
-    return render(request, 'categories_list.html', {'categories': _categories})
+    if request.method == 'POST':
+        if not request.user.is_staff:
+            return HttpResponse('Unauthorized', status=401)
+
+        try:
+            _body = json.loads(request.body)
+            _category = Category()
+            _category.name = _body['name']
+            _category.added_date = timezone.now()
+            _category.save()
+            return HttpResponse('{}', status=200)
+        except Exception as e:
+            return HttpResponse('{}'.format(e), status=400)
+
+    else:
+        _categories = Category.objects.filter()
+        if not _categories.exists():
+            _categories = []
+        return render(request, 'categories_list.html', {'categories': _categories})
+
+
+def iha_category_view(request, id):
+    if request.user.is_anonymous:
+        return redirect('login')
+
+    if request.user.is_staff and request.method == 'PUT':
+        try:
+            _category = get_object_or_404(Category, id=id)
+            _body = json.loads(request.body)
+            _category.name = _body['name']
+            _category.save()
+        except Exception as e:
+            return HttpResponse('{}'.format(e), status=400)
+
+        return HttpResponse('{}', status=200)
+
+    elif request.user.is_staff and request.method == 'DELETE':
+        try:
+            _category = get_object_or_404(Category, id=id)
+            _category.delete()
+        except Exception as e:
+            return HttpResponse('{}'.format(e), status=400)
+
+        return HttpResponse('{}', status=200)
+
+    return HttpResponse('Unauthorized', status=401)
 
 
 def ihas_view(request):
