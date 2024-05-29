86c5c0db9561b13af981588bd1a065a5f24702dd
mucahitmustafa <mucahitmustafay@gmail.com>
2024-05-29 21:14:59 +0300

    Fix categorizied ihas url.

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -15,6 +15,6 @@ urlpatterns = [
     path('categories/<int:id>', views.iha_category_view, name='category'),
 
     path('ihas/', views.ihas_view, name='ihas'),
-    path('ihas/<str:category>', views.categorized_ihas_view, name='categorizied_ihas'),
+    path('ihas/categorizied/<str:category>', views.categorized_ihas_view, name='categorizied_ihas'),
     path('ihas/<int:id>', views.iha_view, name='iha'),
 ]
